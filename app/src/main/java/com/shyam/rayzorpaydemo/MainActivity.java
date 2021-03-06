986bc3224e1dfea98b1dc10eb63f8988510d2d2c
dev-shyam <saxenashyam.srj@gmail.com>
2021-03-06 17:09:00 +0530

    init commit

---
+++ app/src/main/java/com/shyam/rayzorpaydemo/MainActivity.java
@@ -0,0 +1,83 @@
+package com.shyam.rayzorpaydemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.razorpay.Checkout;
+import com.razorpay.PaymentResultListener;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements PaymentResultListener {
+    String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Checkout.preload(getApplicationContext());
+
+        Button button = (Button) findViewById(R.id.btn_pay);
+
+        button.setOnClickListener(v -> startPayment());
+    }
+
+    public void startPayment() {
+        final Activity activity = this;
+
+        final Checkout co = new Checkout();
+
+        try {
+            JSONObject options = new JSONObject();
+            /*
+            options.put("name", "Razorpay Corp");
+            options.put("description", "Demoing Charges");
+            //You can omit the image option to fetch the image from dashboard
+            options.put("image", "https://s3.amazonaws.com/rzp-mobile/images/rzp.png");
+            options.put("currency", "INR");
+            options.put("amount", "100");*/
+            options.put("name", "Bhar Plate");
+            options.put("description", "36Central Training & Development");
+            options.put("image", "https://36central.com/images/project/bharplate_logo.png");
+            options.put("currency", "INR");
+            options.put("amount", "100");
+
+            JSONObject preFill = new JSONObject();
+            // preFill.put("email", "test@razorpay.com");
+            //preFill.put("contact", "9876543210");
+
+            options.put("prefill", preFill);
+
+            co.open(activity, options);
+        } catch (Exception e) {
+            Toast.makeText(activity, "Error in payment: " + e.getMessage(), Toast.LENGTH_SHORT)
+                    .show();
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("unused")
+    @Override
+    public void onPaymentSuccess(String razorpayPaymentID) {
+        try {
+            Toast.makeText(this, "Payment Successful: " + razorpayPaymentID, Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Log.e(TAG, "Exception in onPaymentSuccess", e);
+        }
+    }
+
+    @SuppressWarnings("unused")
+    @Override
+    public void onPaymentError(int code, String response) {
+        try {
+            Toast.makeText(this, "Payment failed: " + code + " " + response, Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Log.e(TAG, "Exception in onPaymentError", e);
+        }
+    }
+}
